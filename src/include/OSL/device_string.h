f307f8512d458e4ef91b0c9198ff3ba96d086a50
Larry Gritz <lg@larrygritz.com>
2023-10-20 17:54:32 -0700

    Make it easier to switch to ustringhash

---
+++ src/include/OSL/device_string.h
@@ -0,0 +1,14 @@
+// Copyright Contributors to the Open Shading Language project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
+
+#pragma once
+
+#include <OSL/hashes.h>
+
+#ifdef OSL_DEPRECATE_PRE_USTRINGHASH
+#warning "include <OSL/device_string.h> should be replaced with <OSL/hashes.h>"
+#endif
+#ifdef OSL_REMOVE_PRE_USTRINGHASH
+#error "include <OSL/device_string.h> should be replaced with <OSL/hashes.h>"
+#endif
